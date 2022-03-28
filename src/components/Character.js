cf098cb69539a95154b5045e68cb4e075a70d72b
Colya Foxworth <cfox1106@gmail.com>
2022-03-28 12:26:40 -0500

    got characters to show up in the DOM

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+import React from 'react';
+
+const Character = (props) => {
+
+    const nameArr = props.list.map(item => item.name);
+    const listNames = nameArr.map((item) => {
+       return <p>{item}</p>
+    });
+    return (
+        <div>{listNames}</div>
+    );
+}
+
+export default Character;
